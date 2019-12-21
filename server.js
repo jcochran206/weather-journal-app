b9e7530e9122301a0f680d4030089d3335b1b89a
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-12-22 00:16:33 +0430

    modified server, added icon, styles to form, and added recent portion

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //empty object
-projectData = {};
+const projectData = {};
 // Require Express to run server and routes
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -18,12 +18,27 @@ app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
+//Get routes
+app.get('/all', sendProdData);
+
+function sendProdData(req, res){
+  console.log('grab data')
+  console.log(projectData)
+
+  res.send(projectData);
+}
+
 //Post routes
-app.post('/', function (req, res) {
-  res.send('POST request to homepage')
-})
-
-
+app.post('/addWeather', addWeather);
+function addWeather(req, res){
+  const newInsert = {
+    temperature: req.body.temperature,
+    date: req.body.date,
+    userRes: req.body.userRes,
+  }
+  projectData = newInsert;
+  console.log(projectData)
+}
 
 //start server
 app.listen(port, () => {
--- website/app.js
+++ website/app.js
@@ -1,4 +1,8 @@
-//skycon icon for displaying conditions for location 
+//skycon icon for displaying conditions for location
 const icon = new Skycons({"color": "#222"});
 icon.set('icon', 'clear-day');
 icon.play();
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
